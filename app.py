595bf1c8cb506cb4997171908c772c4511506fcb
TerTM <155712157+TerTM@users.noreply.github.com>
2025-07-29 16:02:14 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title':'Learner',
+        'location':'Tangerang',
+        'salary':'0'
+    },
+    {
+        'id': 2,
+        'title':'Backend Engineer',
+        'location':'San Francisco, USA',
+        'salary':'$120,000'
+    },
+    {
+        'id':3,
+        'title':'Analyst',
+        'location':'Remote',
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template('home.html', jobs=JOBS) 
+
+@app.route("/api/jobs")
+def job():
+    return jsonify(JOBS)
+
+@app.route("/another")
+def another():
+    return render_template('another.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
